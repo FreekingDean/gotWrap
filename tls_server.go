2f15f31b13e4387e1ee57972baf1689ae36d24e6
Dean <deanfreekinggalvin@gmail.com>
2014-03-16 01:37:05 -0400

    fixed lower upercases

--- tls_server.go
+++ tls_server.go
@@ -40,7 +40,7 @@ func (server *Server) CreateServer() {
 		log.Printf("[gotWrap-SERVER] accepted from %s", conn.RemoteAddr())
 		tlscon, ok := conn.(*tls.Conn)
 		if ok && server.handshake(tlscon.RemoteAddr()) {
-			server.connections[tlscon.RemoteAddr()] = tlscon
+			server.Connections[tlscon.RemoteAddr()] = tlscon
 			go server.handleClient(tlscon.RemoteAddr())
 		} else {
 			conn.Close()
@@ -50,12 +50,12 @@ func (server *Server) CreateServer() {
 }
 
 func (server *Server) handleClient(conn net.Addr) {
-	defer server.connections[conn].Close()
+	defer server.Connections[conn].Close()
 	log.Print("[gotWrap-SERVER] conn: type assert to TLS succeedded")
 	buf := make([]byte, 512)
 	for {
 		log.Print("[gotWrap-SERVER] conn: waiting")
-		n, err := server.connections[conn].Read(buf)
+		n, err := server.Connections[conn].Read(buf)
 		if err != nil {
 			log.Printf("[gotWrap-SERVER] conn: read err: %s", err)
 			break
@@ -63,29 +63,29 @@ func (server *Server) handleClient(conn net.Addr) {
  		log.Printf("[gotWrap-SERVER] conn: read: %s", string(buf[:n]))
  		server.MessageRec(conn, string(buf[:n]))		
 	}
-	delete(server.connections, conn)
+	delete(server.Connections, conn)
 	log.Println("[gotWrap-SERVER] server: conn: closed")
 }
 
 func (server *Server) handshake(conn net.Addr) bool {
-	err := server.connections[conn].Handshake()
+	err := server.Connections[conn].Handshake()
 	if err != nil {
 		log.Fatalf("[gotWrap-SERVER] handshake failed: %s", err)
 		return false
 	} else {
 		log.Print("[gotWrap-SERVER] conn: Handshake completed")
 	}
-	state := server.connections[conn].ConnectionState()
+	state := server.Connections[conn].ConnectionState()
 	log.Println("[gotWrap-SERVER] mutual: ", state.NegotiatedProtocolIsMutual)
 	return true
 }
 
 func (server *Server) SendMessage(conn net.Addr, buf[] byte) {
 	log.Printf("[gotWrap-SERVER] conn: write: %s\n", string(buf))
-	n, err := server.connections[conn].Write(buf)
+	n, err := server.Connections[conn].Write(buf)
 	log.Printf("[gotWrap-SERVER] conn: wrote %d bytes", n)
 	if err != nil {
 		log.Printf("[gotWrap-SERVER] write: %s", err)
-		server.connections[conn].Close()
+		server.Connections[conn].Close()
 	}
 }
